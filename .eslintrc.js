3b456de8e19d0d5e2342669975243ad2831c3abc
Clay Diffrient <clay.diffrient@gmail.com>
2018-01-18 23:46:54 -0700

    Initial setup and landing page filler

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react"
+  ],
+  plugins: ["react", "prettier"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    es6: true,
+    browser: true
+  },
+  rules: {
+    "prettier/prettier": "error"
+  }
+};
---
+++ src/Home/index.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import styles from "./styles.css";
+import Heading from "@instructure/ui-core/lib/components/Heading";
+import Text from "@instructure/ui-core/lib/components/Text";
+import Container from "@instructure/ui-core/lib/components/Container";
+import List, { ListItem } from "@instructure/ui-core/lib/components/List";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className={styles.root}>
+        <Heading level="h1" color="primary-inverse">
+          Flash Cards
+        </Heading>
+
+        <Container as="div" textAlign="start">
+          <Heading color="primary-inverse" margin="0 0 x-small">
+            What is This?
+          </Heading>
+          <Text as="p" textAlign="start" color="primary-inverse">
+            Flash Cards is a web app being developed to help with learning
+            things. {"It's"} being developed by Clay Diffrient as a project for
+            the EDPS 6560 Mulitimedia Learning class in the pursuit of a M.Ed in
+            Instructional Design and Educational Technology.
+          </Text>
+          <Heading color="primary-inverse" margin="0 0 x-small">
+            What Will It Do?
+          </Heading>
+          <Text as="p" textAlign="start" color="primary-inverse">
+            I have somewhat grand aspirations for what it will do eventually,
+            but for now I hope to do the following things:
+          </Text>
+          <List>
+            <ListItem>
+              <Text color="primary-inverse">
+                Show a flash card and flip to the other side via user
+                interaction.
+              </Text>
+            </ListItem>
+            <ListItem>
+              <Text color="primary-inverse">
+                Show a flash card and flip to the other side after a given time.
+              </Text>
+            </ListItem>
+            <ListItem>
+              <Text color="primary-inverse">
+                Track correct vs. incorrect answers by user input
+              </Text>
+            </ListItem>
+            <ListItem>
+              <Text color="primary-inverse">
+                Summarize stats at the end of a session
+              </Text>
+            </ListItem>
+            <ListItem>
+              <Text color="primary-inverse">
+                Allow import/export of flash cards in a file format (likely
+                JSON)
+              </Text>
+            </ListItem>
+          </List>
+        </Container>
+      </div>
+    );
+  }
+}
